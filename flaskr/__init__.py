a2fc6c10dd0f76ab4357b83e20cf63a6af9e329f
bsantanad <0211518@up.edu.mx>
2022-10-06 18:06:50 -0500

    creation of db completed
    now you can do
    flask --app flaskr init-db
    and the tables will be created
    
    Signed-off-by: bsantanad <0211518@up.edu.mx>

---
+++ flaskr/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+import flask
+
+def create_app(test_config = None):
+    # create and config the app
+    app = flask.Flask(
+        __name__, # flaskr
+        instance_relative_config = True, # path relative to instance folder
+    )
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        # load the test config if padded in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # from db.py
+    from . import db
+    db.init_app(app)
+
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
+
+    return app
---
+++ flaskr/db.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+import click
+import flask
+
+def get_db():
+    '''
+    check if db already in flask.g, if not create new
+    connection
+    '''
+    if 'db' not in flask.g:
+        flask.g.db = sqlite3.connect(
+            flask.current_app.config['DATABASE'],
+            detect_types = sqlite3.PARSE_DECLTYPES,
+        )
+        # return rows that behave like dicts
+        flask.g.row_factory = sqlite3.Row
+    return flask.g.db
+
+def close_db(e = None):
+    db = flask.g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with flask.current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+# click defines a command line command
+@click.command('init-db')
+def init_db_command():
+    '''
+    clear the exsisting data and create new tables
+    '''
+    init_db()
+    click.echo('init the db')
+
+def init_app(app):
+    # teardown tells flask to call this when cleaning up after returning a
+    # response
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
